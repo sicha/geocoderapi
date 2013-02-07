f30aabaa94d132682fb4e5299c0c7bbb4f21851f
sicha <TheSicha@gmail.com>
2013-02-07 14:24:41 +0100

    initial import

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  def index
+
+  end
+
+  def pricing
+
+  end
+
+  def api
+
+  end
+
+  def contact
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoCoderAPI::Application.config.secret_token = 'a98337bc7f35defc5947af52ed393985deefbd31daea38070bee4e70d7379257297cbb4358bef6782b2ad49606b65a1192c9cab85da67b56a3f9d5a82b0cc116'
